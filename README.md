0394fff62a2e91a67619875ab97e1c84f897b3c1
Sebastian Kargl <kargl121@gmail.com>
2022-02-03 01:36:59 +0100

    Bot is working!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A simple solution to play music on your discord server!
 developed primarily to server private needs on my friends server.
+
+**remmeber to have ffmpeg installed on the host system**
--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import os
-import youtube_dl
+import music
+
 
 def print_hi(name):
     load_dotenv()
     if not os.getenv('BOT_API_TOKEN'):
         print("NO BOT_API_TOKEN found in .env")
         return
+
     BOT_API_TOKEN = os.getenv('BOT_API_TOKEN')
     client = commands.Bot(command_prefix='?', intents=discord.Intents.all())
+
+    cogs = [music]
+
+    for i in range(len(cogs)):
+        cogs[i].setup(client)
+
     client.run(BOT_API_TOKEN)
 
 
